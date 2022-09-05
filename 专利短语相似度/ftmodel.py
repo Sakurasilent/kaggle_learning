ba293fdae46e0bf19ccb2c44d98396f4f567535f
Administrator <492627639@qq.com>
2022-09-05 16:21:39 +0800

    更新nbme的代码

--- 专利短语相似度/ftmodel.py
+++ 专利短语相似度/ftmodel.py
@@ -1,5 +1,32 @@
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
 from utils import *
-num_labels = 1
+from dataprocess import TrainDataset
 num_labels = 1
 model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=num_labels)
+metric_name = "person"
+batch_size = 100
+model_name = model_checkpoint.split("/")[-1]
+args = TrainingArguments(
+    f"{model_name}-finetuned",
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model=metric_name,
+    save_total_limit=1
+)
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    return metric.compute(predictions=predictions, references=labels)
+
+
+
+
+
+
--- 专利短语相似度/utils.py
+++ 专利短语相似度/utils.py
@@ -1,1 +1,7 @@
+from datasets import load_metric
+
+metric = load_metric('glue', 'stsb')
 model_checkpoint = 'microsoft/deberta-v3-small'
+
+if __name__ == '__main__':
+    print(metric)
